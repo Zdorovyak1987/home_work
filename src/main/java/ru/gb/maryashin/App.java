c159b74fa6d3b975d6ef0911ecb65e8fd67ccf2c
Zdorovyak <prk87@ya.ru>
2022-03-03 19:29:40 +0300

    Initial commit

---
+++ src/main/java/ru/gb/maryashin/App.java
@@ -0,0 +1,9 @@
+package ru.gb.maryashin;
+
+
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/ru/gb/maryashin/HomeWorkApp.java
@@ -0,0 +1,37 @@
+package ru.gb.maryashin;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange\nBanana\nAppel");
+    }
+
+    public static void checkSumSign() {
+        int a = 5, b = 15;
+        int c = a + b;
+        System.out.println("Сумма " + (c >= 0 ? "положительная" : "отрицательная"));
+        }
+
+    public static void printColor(){
+        int value = -57;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value <= 100){
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers(){
+        int a = 75, b = 35;
+        System.out.println( (a >= b ? "a>=b" : "a<b"));
+    }
+    }
+
